72f7bc0930bed99ef7285708a1ad21dbb6da3a9c
Klap.ir <mohammadrezaasiyaban2286@gmail.com>
2023-04-27 23:29:19 +0430

    To Do

--- app/src/main/java/com/example/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/todoapplication/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.todoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.todoapplication.Utels.DataBaseHelper;
+import com.example.todoapplication.adapter.ToDoAdapter;
+import com.example.todoapplication.model.ToDoModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnDialogClaseListne{
+
+    private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
+    private DataBaseHelper myDB;
+    private List<ToDoModel>mList;
+    private ToDoAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView=findViewById(R.id.recyclerview);
+        fab=findViewById(R.id.fab);
+        myDB=new DataBaseHelper(MainActivity.this);
+        mList=new ArrayList<>();
+        adapter=new ToDoAdapter(myDB,MainActivity.this);
+
+        mList=myDB.getAllTask();
+        Collections.reverse(mList);
+        adapter.setTask(mList);
+
+        fab.setOnClickListener(view -> {
+            AddNowTask.newInstance().show(getSupportFragmentManager(),AddNowTask.TAG);
+        });
+        ItemTouchHelper itemTouchHelper=new ItemTouchHelper(new RecyclerViewTouchHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onDialogClass(DialogInterface dialogInterface) {
+        mList=myDB.getAllTask();
+        Collections.reverse(mList);
+        adapter.setTask(mList);
+        adapter.notifyDataSetChanged();
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/todoapplication/OnDialogClaseListne.java
@@ -0,0 +1,7 @@
+package com.example.todoapplication;
+
+import android.content.DialogInterface;
+
+public interface OnDialogClaseListne {
+    void onDialogClass(DialogInterface dialogInterface);
+}
